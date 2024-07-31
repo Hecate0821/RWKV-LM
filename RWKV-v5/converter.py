98f7df7f400fe995b99724891d491a9a03aaa9c6
Hecate <hecate0821@outlook.com>
2024-07-31 11:31:32 +0800

    fix: memory error

--- RWKV-v5/converter.py
+++ RWKV-v5/converter.py
@@ -5,8 +5,10 @@ import json
 import argparse
 from tqdm import tqdm
 import gc
+from memory_profiler import profile
 
 
+@profile
 def convert_parquet_to_jsonl(parquet_file, output_file):
     # 使用pyarrow逐行读取Parquet文件
     table = pq.read_table(parquet_file)
@@ -32,7 +34,7 @@ def convert_parquet_to_jsonl(parquet_file, output_file):
     del df
     gc.collect()
 
-
+@profile
 def process_directory(root_dir, output_file):
     # 不删除，接着往里写就行
     # 如果output.jsonl文件已经存在，则先删除它
